5ffd4e1049949830d3efe77b00884e564a29098b
wulnm <wumqing@yeah.net>
2021-05-30 22:10:19 +0800

    add log4j

--- src/main/java/com/xWash/aspect/CookiesAspect.java
+++ src/main/java/com/xWash/aspect/CookiesAspect.java
@@ -1,22 +1,19 @@
 package com.xWash.aspect;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.xWash.dao.UserDao;
 import com.xWash.entity.Record;
 import com.xWash.util.CookieUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 
 @Component("cookiesAspect")
 @Aspect
@@ -25,6 +22,8 @@ public class CookiesAspect {
     @Qualifier("userMapper")
     Object userMapper;
 
+    Logger logger = LogManager.getLogger("sqlLog");
+
     @Pointcut("execution(* com.xWash.admin.CheckController.getMachineStatus(..))")
     private void pt4checkController() {
     }
@@ -32,29 +31,31 @@ public class CookiesAspect {
     @Before("pt4checkController() && args(building,autoUpdate,userCookieStr,*,response,..)")
     private void beforeCheck(String building, String autoUpdate, String userCookieStr
             , HttpServletResponse response) {
-
-        System.out.println(userMapper);
-        UserDao userDao = (UserDao) userMapper;
         Cookie userCookie;
-        if (userCookieStr == null || userCookieStr.equals("") || userDao.getUserByCookie(userCookieStr) == null) {
-            // 无cookie ， 需要新建cookie
-            do {
-                userCookieStr = CookieUtil.generateRandUserCookie();
-            } while (userDao.getUserByCookie(userCookieStr) != null);
-            userCookie = new Cookie("user", userCookieStr);
-            userCookie.setMaxAge(60 * 60 * 24 * 365 * 4);
-            userCookie.setPath("/");
-            response.addCookie(userCookie);
-            userDao.addUser(userCookieStr);
+        try{
+            UserDao userDao = (UserDao) userMapper;
+            if (userCookieStr == null || userCookieStr.equals("") || userDao.getUserByCookie(userCookieStr) == null) {
+                // 无cookie ， 需要新建cookie
+                do {
+                    userCookieStr = CookieUtil.generateRandUserCookie();
+                } while (userDao.getUserByCookie(userCookieStr) != null);
+                userCookie = new Cookie("user", userCookieStr);
+                userCookie.setMaxAge(60 * 60 * 24 * 365 * 4);
+                userCookie.setPath("/");
+                response.addCookie(userCookie);
+                userDao.addUser(userCookieStr);
+            }
+            // 记录本次访问
+            Record record = new Record();
+            record.setBuilding(building);
+            record.setCookie(userCookieStr);
+            if (autoUpdate != null)
+                record.setMode("autoUpdate");
+            else
+                logger.info(userCookieStr + " comes");
+            userDao.addRecord(record);
+        }catch(Exception e){
+            logger.warn(e.getMessage());
         }
-        // 记录本次访问
-        Record record = new Record();
-        record.setBuilding(building);
-        record.setCookie(userCookieStr);
-        if (autoUpdate != null)
-            record.setMode("autoUpdate");
-        userDao.addRecord(record);
-
-
     }
 }
--- src/main/java/com/xWash/service/Impl/Distributor.java
+++ src/main/java/com/xWash/service/Impl/Distributor.java
@@ -4,13 +4,17 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import com.xWash.entity.MStatus;
 import com.xWash.service.IChecker;
 import com.xWash.util.BuildingFileUtil;
 import com.xWash.entity.QueryResult;
 import com.xWash.service.IDistributor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.xml.ws.http.HTTPException;
 import java.io.File;
 import java.util.LinkedHashMap;
 
@@ -22,6 +26,7 @@ public class Distributor implements IDistributor {
     final UCleanAPPChecker uCleanAPPChecker;
     final SodaChecker sodaChecker;
     final LocationDealer locationDealer;
+    Logger logger = LogManager.getLogger("checkerLog");
 
     public Distributor(@Autowired UCleanChecker uCleanChecker, @Autowired UCleanAPPChecker uCleanAPPChecker, @Autowired SodaChecker sodaChecker, @Autowired LocationDealer locationDealer) {
         this.uCleanChecker = uCleanChecker;
@@ -38,11 +43,19 @@ public class Distributor implements IDistributor {
             checker = uCleanChecker;
         } else if (belong.equals("sodalife")) {
             checker = sodaChecker;
-        } else if (belong.equals("UCleanAPP")){
+        } else if (belong.equals("UCleanAPP")) {
             checker = uCleanAPPChecker;
         }
         assert checker != null;
-        return checker.checkByQrLink((String) json.get("qrLink"));
+        QueryResult result = QueryResult.getEmptyInstance();
+        try {
+            result = checker.checkByQrLink((String) json.get("qrLink"));
+        }catch (HTTPException e){
+            result.setStatus(MStatus.UNKNOWN);
+            result.setMessage("网络错误！请稍后查看");
+            logger.warn(json.get("name") + " -> ("+ e.getStatusCode() +") " + e.getMessage());
+        }
+        return result;
     }
 
     /**
@@ -82,6 +95,7 @@ public class Distributor implements IDistributor {
     /**
      * 使用Hutool的Json处理
      * 最终真正实现查询的方法
+     *
      * @param jsonObj json对象
      * @return 查询结果Map key:洗衣机名  value: 洗衣机状态
      */
