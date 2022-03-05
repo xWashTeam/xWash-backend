3a7d0c599a053de9329defd4577cbb877132df2d
wulnm <wumqing@yeah.net>
2022-03-05 14:05:05 +0800

    feat: add logger for exception

--- src/main/java/com/xWash/aspect/CheckerAspect.java
+++ src/main/java/com/xWash/aspect/CheckerAspect.java
@@ -1,41 +1,46 @@
-//package com.xWash.aspect;
-//
-//import com.xWash.model.entity.QueryResult;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.*;
-//import org.springframework.stereotype.Component;
-//
-//
-//@Aspect
-//@Component("checkerAspect")
-//public class CheckerAspect {
-//    final Logger logger = LogManager.getLogger("checkerLog");
-//
-//    @Pointcut("execution(* com.xWash.service.intf.IChecker.checkByQrLink(..))")
-//    public void netRequest() { }
-//
-//    @Around(value = "netRequest()")
-//    public Object logAround(ProceedingJoinPoint jp){
-//        Object result=null;
-//        String qrLink = (String) (jp.getArgs()[0]);
-//        try {
-//            result = jp.proceed();
-//            QueryResult qs = (QueryResult) result;
-//            if (qs.isInit()){
-//                synchronized (logger){
-//                    logger.info("qrLink -> " + qrLink + ", QueryResult -> " + result);
-//                }
-//            }
-//        } catch (Throwable throwable) {
-//            synchronized (logger){
-//                logger.warn("qrLink -> "+ qrLink + ", msg -> " + throwable.getMessage());
-//            }
-//        }finally {
-//            return result;
-//        }
-//
-//    }
-//}
-//
+package com.xWash.aspect;
+
+import cn.hutool.core.io.IORuntimeException;
+import cn.hutool.http.HttpResponse;
+import com.xWash.model.dao.Machine;
+import com.xWash.model.entity.MessageEnum;
+import com.xWash.model.entity.QueryResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component("checkerAspect")
+public class CheckerAspect {
+    final Logger logger = LogManager.getLogger("checkerLog");
+
+    @Pointcut("execution(* com.xWash.service.Impl.AbstractChecker.check(..))")
+    public void checkPoint() {
+    }
+
+    /**
+     * Around weave for request() to catch exception and specific response
+     *
+     * @param jp
+     * @return
+     */
+    @Around(value = "checkPoint()")
+    public Object aroundRequest(ProceedingJoinPoint jp) {
+        Machine machine = (Machine) (jp.getArgs()[0]);
+        QueryResult qr = new QueryResult();
+        try {
+            qr = (QueryResult) jp.proceed();
+            if (!qr.isNormal()) {
+                logger.warn(machine.getName() + ": " + qr.getMessage());
+            }
+        } catch (Exception e) {
+            logger.error(machine.getName() + ": " + e.getCause());
+            logger.error(e.getMessage());
+        } finally {
+            return qr;
+        }
+    }
+}
--- src/main/java/com/xWash/controller/CheckController.java
+++ src/main/java/com/xWash/controller/CheckController.java
@@ -22,8 +22,6 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "/api", method = {RequestMethod.GET, RequestMethod.POST})
 public class CheckController {
-
-
     @Autowired
     Distributor distributor;
     @Autowired
