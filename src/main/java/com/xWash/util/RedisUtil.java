0602c941b943776c9a7383fb017b65d58bb2405f
wulnm <wumqing@yeah.net>
2022-03-05 23:58:25 +0800

    fix: fix jedis connections exhausted

--- src/main/java/com/xWash/util/RedisUtil.java
+++ src/main/java/com/xWash/util/RedisUtil.java
@@ -1,13 +1,9 @@
 package com.xWash.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-
-import javax.annotation.Resource;
 import java.util.Map;
 
 @Component
@@ -17,31 +13,41 @@ public class RedisUtil {
 
     public boolean exist(String key) {
         Jedis jedis = jedisPool.getResource();
-        return jedis.exists(key);
+        boolean exist = jedis.exists(key);
+        jedis.close();
+        return exist;
     }
 
     public Map<String, String> hashGetAll(String key) {
         Jedis jedis = jedisPool.getResource();
-        return jedis.hgetAll(key);
+        Map<String, String> map = jedis.hgetAll(key);
+        jedis.close();
+        return map;
     }
 
     public String hashGet(String key, String field) {
         Jedis jedis = jedisPool.getResource();
-        return jedis.hget(key, field);
+        String value =  jedis.hget(key, field);
+        jedis.close();
+        return value;
     }
 
     public void hashSet(String key, String field, String value) {
         Jedis jedis = jedisPool.getResource();
         jedis.hset(key, field, value);
+        jedis.close();
     }
 
     public void setStr(String key, String value) {
         Jedis jedis = jedisPool.getResource();
         jedis.set(key, value);
+        jedis.close();
     }
 
     public String getStr(String key) {
         Jedis jedis = jedisPool.getResource();
-        return jedis.get(key);
+        String value = jedis.get(key);
+        jedis.close();
+        return value;
     }
 }
